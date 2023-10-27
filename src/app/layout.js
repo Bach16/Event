8deb2e825564e92e7a4b4c721e7a609f88a65a72
Bach16 <gwyn56717@gmail.com>
2023-10-27 05:08:26 -0500

    update landing page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,13 @@
+import Footer from "./components/sections/Footer";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={` bg-[#f2f2f2]`}>{children}</body>
+      <body className={` bg-[#f2f2f2]`}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
