4a8da988fcf293f6e7eba71e9dd34521863e2d10
Bach16 <gwyn56717@gmail.com>
2023-10-28 22:55:07 -0500

    Update FirstSection.jsx

--- src/app/components/sections/FirstSection.jsx
+++ src/app/components/sections/FirstSection.jsx
@@ -1,37 +1,54 @@
-import { Lobster } from "next/font/google";
+import capitalize from "@/assets/capitalize";
+import { Lobster, Marcellus } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 
 const lobster = Lobster({
   subsets: ["latin"],
   display: "swap",
   weight: ["400"],
 });
+const marcellus = Marcellus({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400"],
+});
 
-const FirstSection = ({empresa}) => {
+const FirstSection = ({ empresa }) => {
   return (
     <div
-      className={`  bg-[url('../../public/photos-by-lanty-O38Id_cyV4M-unsplash.jpg')] bg-no-repeat bg-fixed bg-cover `}
+      className={`  bg-[url('../../public/al-elmes-ULHxWq8reao.jpg')] bg-no-repeat bg-fixed bg-cover `}
     >
       <div
         className={`h-[30rem] sm:h-screen bg-black/[0.4] backdrop-brightness-75`}
       >
-        <nav className="h-[10%] sm:flex sm:justify-center">
+        <nav className="h-[15%] sm:flex sm:justify-center">
           <ul className="flex justify-center items-center sm:w-[90%] sm:p-[2rem] px-[1rem]">
-            <li className="text-[#ffffff]">{empresa?.length ? empresa: "Empresa"}</li>
-            
+            <li className={`text-[#ffffff] text-[2rem] ${marcellus.className}`}>
+              {empresa?.length ? capitalize(empresa) : "Empresa"}
+            </li>
           </ul>
         </nav>
-        <div className="flex h-[7%] sm:h-[25%]" />
+        <div className="flex h-[7%] sm:h-[15%]" />
         <div className="h-[80%] sm:h-[40%] flex flex-col justify-around items-center">
           <div className="flex flex-col justify-between items-center">
             <p className="font-light leading-[2rem] text-[1.14rem] sm:text-[1.25rem]  text-center font-platNomor text-[#ffffff]">
               Creamos experiencias inolvidables.
             </p>
-            <h1 className={`font-regular leading-[3rem] sm:leading-[4rem] sm:text-[6rem] text-[3.8rem] text-center ${lobster.className} text-[#ffffff]`}>
+            <h1
+              className={`font-regular leading-[3rem] sm:leading-[4rem] sm:text-[6rem] text-[3.8rem] text-center ${lobster.className} text-[#ffffff]`}
+            >
               Hacemos realidad tus eventos
             </h1>
           </div>
-          <button className="sm:w-[15%] w-[85%] border-[0.5px] border-solid border-[#222222] font-semibold h-[2.3rem] rounded-full bg-[#ffffff]">
-            Cotiza tu evento
+          <button className="sm:w-[15%] w-[85%] font-semibold h-[2.3rem] rounded-full bg-[#ffffff]">
+            <Link
+              href={"https://api.whatsapp.com/send?phone=593993272165"}
+              target="_blank" rel="noopener noreferrer"
+              className=" flex w-[100%] h-[100%] justify-center items-center"
+            >
+              Cotiza tu evento
+            </Link>
           </button>
         </div>
       </div>
