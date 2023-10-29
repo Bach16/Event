48d05d697dcc9bf1dd07376b1990d023bf7e9c0c
Bach16 <gwyn56717@gmail.com>
2023-10-28 22:55:22 -0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,10 @@
-import Footer from "./components/sections/Footer";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={` bg-[#f2f2f2]`}>
-        {children}
-        <Footer />
+        {children}        
       </body>
     </html>
   );
