ac2ab7a8d2899e67089d3a061a94392a81fc54ba
Bach16 <gwyn56717@gmail.com>
2023-10-29 18:51:54 -0500

    Update Card.jsx

--- src/app/components/cards/Card.jsx
+++ src/app/components/cards/Card.jsx
@@ -1,11 +1,19 @@
+import { Merriweather } from "next/font/google";
+
+
+const merriweather = Merriweather({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400"],
+});
 
 const Card = ({name,img}) => {
   return (
     <div
-      className={` bg-center bg-no-repeat bg-cover h-[22rem] sm:h-[30rem] w-[15rem] sm:w-[15rem] `}
+      className={` bg-center bg-no-repeat bg-cover ${merriweather.className} h-[22rem] sm:h-[30rem] w-[15rem] sm:w-[15rem] `}
       style={{ backgroundImage: `url(${img.src})`}}
     >
-      <div className="h-[100%] w-[100%] font-[10] font-bell text-white flex justify-center items-center  bg-black/[0.4] backdrop-brightness-75">
+      <div className="h-[100%] w-[100%] font-[10] text-white flex justify-center items-center  bg-black/[0.4] backdrop-brightness-75">
         <h4 className=" text-[3.1rem] ">{name}</h4>
       </div>
     </div>
