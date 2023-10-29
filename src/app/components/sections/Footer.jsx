f9ecd139301e5ce591967554d226c2f6154e7588
Bach16 <gwyn56717@gmail.com>
2023-10-28 22:55:09 -0500

    Update Footer.jsx

--- src/app/components/sections/Footer.jsx
+++ src/app/components/sections/Footer.jsx
@@ -1,6 +1,7 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 import { Abel } from "next/font/google";
+import capitalize from "@/assets/capitalize";
 
 const abel = Abel({
   subsets: ["latin"],
@@ -8,17 +9,33 @@ const abel = Abel({
   weight: ["400"],
 });
 
-const Footer = () => {
-
-  
+const Footer = ({empresa}) => {
   return (
-    <footer className={` border-[1px] ${abel.className} border-solid border-t-[#c1c1c1] text-[#222222] h-[7rem] font-bell flex justify-center items-center`}>
-        <p>© 2023 Empresa | Hecho por 
-          <Link href="https://www.linkedin.com/in/ismael-rojas-villacreses-583438252/" target="_blank" rel="noopener noreferrer"> Mateo Rojas</Link>
-          <Link href="https://www.linkedin.com/in/dave-rojas-villacreses/" target="_blank" rel="noopener noreferrer"> y David Rojas</Link>
-        </p>
+    <footer
+      className={` border-[1px] ${abel.className} border-solid border-t-[#c1c1c1] text-[#222222] h-[7rem] font-bell flex flex-col sm:flex-row justify-center items-center`}
+    >
+      <p  >© 2023 {empresa?.length ? capitalize(empresa) : "Empresa"} | Hecho por </p>
+      <div>
+        <Link
+          href={"https://api.whatsapp.com/send?phone=593993272165"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="sm:ml-[0.2rem]"
+        >
+          {" "}
+          Mateo Rojas
+        </Link>
+        <Link
+          href={"https://api.whatsapp.com/send?phone=593993272165"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          y David Rojas
+        </Link>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
