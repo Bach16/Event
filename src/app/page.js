38889fd97d1cdc74043ad741f81fae9b3c498d1a
Bach16 <gwyn56717@gmail.com>
2023-10-28 22:55:26 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,14 @@
-"use client"
-import { useParams, useRouter } from "next/navigation";
-import { FirstSection, SecondSection, ThirdSection } from "./components";
+import { FirstSection, Footer, SecondSection, ThirdSection } from "./components";
 
 export default function Home() {
-  const params = useParams();
-  console.log(params);
+
 
   return (
     <>
-      <FirstSection empresa={params?.slug} />
-      <SecondSection empresa={params?.slug}/>
-      <ThirdSection empresa={params?.slug}/>
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <Footer/>
     </>
   );
 }
