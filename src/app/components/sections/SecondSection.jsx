a84a3231b665d2d1e1f83c547a87fc68c5ed2435
Bach16 <gwyn56717@gmail.com>
2023-10-28 22:55:12 -0500

    Update SecondSection.jsx

--- src/app/components/sections/SecondSection.jsx
+++ src/app/components/sections/SecondSection.jsx
@@ -1,12 +1,20 @@
+import { Marcellus } from "next/font/google";
 import { CardContainer } from "..";
+import capitalize from "@/assets/capitalize";
+
+const marcellus = Marcellus({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400"],
+});
 
 const SecondSection = ({empresa}) => {
   return (
     <div className="h-[87rem] flex justify-center items-center sm:h-[50rem] ">
       <div className=" flex flex-col justify-around items-center h-[90%] sm:h-[80%]">
         <div className="h-[5%] w-[100%] sm:h-[1%] flex flex-col justify-center items-center">
-          <h3 className="font-regular leading-[1rem]  sm:text-[1.2rem] text-[1.2rem]  text-[#272727]">
-            {empresa?.length ? empresa: "Empresa"}
+          <h3 className={`font-regular leading-[1rem] ${marcellus.className}  sm:text-[1.2rem] text-[1.2rem]  text-[#272727]`}>
+            {empresa?.length ? capitalize(empresa): "Empresa"}
           </h3>
           <h3 className="font-[1000] leading-[2.8rem] text-center sm:leading-[3rem] sm:text-[3rem] text-[3rem] font-bell text-[#222222]">
             Eventos a tu Medida
