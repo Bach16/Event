b70ade1da3f77952b05de243c3a858d1b1b50b9a
Bach16 <gwyn56717@gmail.com>
2023-10-28 22:55:03 -0500

    Update index.js

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,5 +1,6 @@
 export { default as Card } from './cards/Card';
 export { default as CardContainer } from './cards/CardContainer';
 export { default as FirstSection } from './sections/FirstSection';
+export { default as Footer } from './sections/Footer';
 export { default as SecondSection } from './sections/SecondSection';
 export { default as ThirdSection } from './sections/ThirdSection';
